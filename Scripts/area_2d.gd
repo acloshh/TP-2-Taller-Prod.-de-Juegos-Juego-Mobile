168b8d3fc798ccdcf307e6c50efa628292338e16
acloshh <santitornau@gmail.com>
2026-06-23 00:00:48 -0300

    Se agregaron cosas, que no recuerdos cuales son, lamentablemente

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -2,20 +2,24 @@ extends Area2D
 
 signal moneda_recogida
 
-var jugador_objetivo: Node2D = null
-var velocidad_atraccion: float = 600.0
+# --- VARIABLES DEL IMÁN ---
+var objetivo_iman: Node2D = null
+var velocidad_atraccion: float = 1000.0 # Velocidad de arranque
+var aceleracion_atraccion: float = 6000.0 # Aceleración por segundo
 
 func _process(delta: float) -> void:
-	# Si un jugador la está atrayendo, la moneda se mueve hacia él
-	if jugador_objetivo != null:
-		global_position = global_position.move_toward(jugador_objetivo.global_position, velocidad_atraccion * delta)
+	# Si el imán la enganchó, acelera y persigue al jugador
+	if objetivo_iman != null:
+		velocidad_atraccion += aceleracion_atraccion * delta
+		global_position = global_position.move_toward(objetivo_iman.global_position, velocidad_atraccion * delta)
 
-# Función que va a llamar el imán del jugador para "enganchar" la moneda
-func ser_atraida(jugador: Node2D) -> void:
-	jugador_objetivo = jugador
+# El personaje llama a esta función con su Área2D del imán
+func ser_atraida(personaje: Node2D) -> void:
+	objetivo_iman = personaje
 
+# Cuando choca físicamente contra el personaje
 func _on_body_entered(body: Node2D) -> void:
-	# Acá detecta cuando el jugador la toca físicamente para sumarla
-	if body.name == "Jugador" or body is CharacterBody2D: 
+	# Verificamos que sea el jugador (usando un método que sepamos que tiene el personaje)
+	if body.has_method("activar_iman"):
 		moneda_recogida.emit()
 		queue_free()
